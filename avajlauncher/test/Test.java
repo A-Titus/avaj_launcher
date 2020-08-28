585e7ba551485880d7b23ad8a979f4aa486355b2
TITUSA <abdussamadtitus@gmail.com>
2020-08-28 10:38:05 +0200

    fixed folder structure

---
+++ avajlauncher/test/Test.java
@@ -0,0 +1,7 @@
+package avajlauncher.test;
+
+public class Test{
+	public void printHello(){
+		System.out.println("hello test");
+	}
+}
--- src/avajlauncher/test/Test.java
+++
@@ -1,7 +0,0 @@
-package avajlauncher.test;
-
-public class Test{
-	public void printHello(){
-		System.out.println("hello test");
-	}
-}
